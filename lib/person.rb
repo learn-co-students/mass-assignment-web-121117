79a0c7f8de490feb4bb8aa6320d41127531a46f5
github username <github email address>
2017-12-16 21:37:08 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,7 @@
+require 'pry'
+
 class Person
+
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
@@ -9,3 +12,24 @@ class Person
     end
   end
 end
+
+ 
+# avi = {
+#   :name => "Avi",
+#   :birthday => "01/29/1984",
+#   :hair_color => "brown",
+#   :eye_color => "brown",
+#   :height => "short",
+#   :weight => "good",
+#   :handed => "lefty",
+#   :complexion => "decent",
+#   :t_shirt_size => "medium",
+#   :wrist_size => "small",
+#   :glove_size => "normal",
+#   :pant_length => "32",
+#   :pant_width => "32"
+# }
+#
+# avi_person = Person.new(avi)
+
+# binding.pry
